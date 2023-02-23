ef8073f4d2f84949652c4893eb6bd99961c47d7c
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-23 11:39:23 -0500

    thank god

--- achieve_7.py
+++ achieve_7.py
@@ -1,9 +1,9 @@
 course = {}
 course = {
-    "English":"ENG1221",
-    "Careers":"CRE1342",
-    "Networking Basics":"NTB3070",
-    "Sociology":"SCL1020"
+    "ENG1221":"English",
+    "CRE1342":"Careers",
+    "NTB3070":"Networking Basics",
+    "SCL1020":"Sociology"
 }
 stuInfo = {}
 firstN = input("Enter your first name:")
@@ -15,7 +15,28 @@ stuInfo["Student #"] = studN
 print(list(course),"\nSelect your courses by entering any of these course codes: ENG1221, CRE1342 , NTB3070, SCL1020\nSeperate your choices with a ','")
 courSel = input().split(",")
 stuInfo["Course selection"] = courSel
-print("Your course registration summary:\n",(stuInfo))
+print("Your course registration summary:\n")
+print(stuInfo.get("First name"))
+print(stuInfo.get("Last name"))
+for x in courSel:
+    print(course.get(x))
+    
+
+
+
+    
+   
+
+
+
+
+    
+
+    
+
+
+
+
 
 
 
