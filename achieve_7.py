d5ca7cbc85c5b4f543721352b61e9600f70e6504
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-23 09:41:30 -0500

    Progress

--- achieve_7.py
+++ achieve_7.py
@@ -1,1 +1,21 @@
+course = {}
+course = {
+    "English":"ENG1221",
+    "Careers":"CRE1342",
+    "Networking Basics":"NTB3070",
+    "Sociology":"SCL1020"
+}
+stuInfo = {}
+firstN = input("Enter your first name:")
+stuInfo["First name"] = firstN
+lastN = input("Enter your last name:")
+stuInfo["Last name"] = lastN
+studN = input("Enter your student number:")
+stuInfo["Student #"] = studN
+print(list(course),"\nSelect your courses by entering any of these course codes: ENG1221, CRE1342 , NTB3070, SCL1020\nSeperate your choices with a ','")
+courSel = input().split(",")
+
+
+
+
 
